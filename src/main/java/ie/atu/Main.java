042661ff9883fbf3cda252ea7e66ecb29e75a31d
DenysKapustianskyi-S <G00470377@atu.ie>
2025-10-01 17:19:11 +0100

    new method to subtract two numbers

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -4,17 +4,45 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
         // Create an instance of Calculator
         Calculator calc = new Calculator();
-        System.out.println("Simple Calculator - Add Two Numbers");
-        System.out.print("Enter first number: ");
-        int a = sc.nextInt();
-        System.out.print("Enter second number: ");
-        int b = sc.nextInt();
-        int result = calc.add(a, b);
-        System.out.println("Result: " + result);
-        sc.close();
 
+        Scanner scan1 = new Scanner(System.in);     //an object of scanner
+
+        System.out.print("Choose an operation (add, subtract, multiply, divide): ");
+        String operation = scan1.next();    //this variable saves user's input for type of calculation
+
+        System.out.println("Please enter a number: ");
+        int firstNum = scan1.nextInt();       //fist number variable
+        System.out.println("you entered " + firstNum);
+
+
+        System.out.println("Please enter a second number:" );
+        int secondNum = scan1.nextInt();  //second number variable
+        System.out.println("You entered " + secondNum);
+
+        boolean validOperation = true;
+        int result = switch (operation) {
+            case "add" -> {     //this code is for adding
+                yield calc.add(firstNum, secondNum);
+            }
+            case "subtract" -> {        //this one is for subtraction
+                yield calc.subtract(firstNum, secondNum);
+            }
+            case "multiply" -> {        //this one is for multiplying
+                yield firstNum * secondNum;
+            }
+            case "divide" -> {          //this one is for dividing
+                yield firstNum / secondNum;
+            }
+            default -> {
+                System.out.println("Invalid operation '" + operation +"'"); //default code in case of incorrect input
+                validOperation = false;
+                yield 0;
+            }
+        };
+        if(validOperation){         //if operation is not valid this code will not run
+            System.out.println("The result is: " + result);
+        }
     }
 }
